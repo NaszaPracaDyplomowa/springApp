9e3dfbb9c30f7c2c6194eb7cf4eb58fdeba20f2a
NaszaPracaDyplomowa <Nasza.Praca.Dyplomowa@gmail.com>
2017-02-27 17:39:09 +0100

    mysql -> postgres

--- db/mysql.sql
+++ db/mysql.sql
@@ -1,5 +1,5 @@
 
-CREATE  TABLE users (
+/*CREATE  TABLE users (
   username VARCHAR(45) NOT NULL ,
   password VARCHAR(45) NOT NULL ,
   enabled TINYINT NOT NULL DEFAULT 1 ,
@@ -14,6 +14,39 @@ CREATE TABLE user_roles (
   KEY fk_username_idx (username),
   CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username));
 
+INSERT INTO users(username,password,enabled)
+VALUES ('mkyong','123456', true);
+INSERT INTO users(username,password,enabled)
+VALUES ('alex','123456', true);
+
+INSERT INTO user_roles (username, role)
+VALUES ('mkyong', 'ROLE_USER');
+INSERT INTO user_roles (username, role)
+VALUES ('mkyong', 'ROLE_ADMIN');
+INSERT INTO user_roles (username, role)
+VALUES ('alex', 'ROLE_USER');*/
+CREATE  TABLE users (
+  user_id bigserial not null, 
+  username varchar(45) not null,
+  password varchar(45) not null,
+  enabled boolean not null default true,
+  constraint users_pkey primary key (user_id),
+  constraint  uni_username UNIQUE (username)
+  )
+
+CREATE TABLE user_roles (
+  user_role_id bigserial NOT NULL ,
+  username varchar(45) NOT NULL,
+  role varchar(45) NOT NULL,
+  PRIMARY KEY (user_role_id),
+  CONSTRAINT uni_username_role UNIQUE (role,username),
+  CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username))
+
+CREATE INDEX username_idx
+  ON user_roles
+  USING btree
+  (username);
+  
 INSERT INTO users(username,password,enabled)
 VALUES ('mkyong','123456', true);
 INSERT INTO users(username,password,enabled)
